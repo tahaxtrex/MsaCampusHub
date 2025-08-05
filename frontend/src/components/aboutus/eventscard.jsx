02aea69225966c27754fcf897c03cfd156fa5258
tahaxtrex <hbirritaha@gmail.com>
2025-08-05 23:47:29 +0100

    add profile page

--- frontend/src/components/aboutus/eventscard.jsx
+++ frontend/src/components/aboutus/eventscard.jsx
@@ -82,8 +82,14 @@ void main() {
     
     st = st * cellSize + cellOffset;
     
-    outColor = texture(uTex, st);
-    outColor.a *= vAlpha;
+    vec4 texColor = texture(uTex, st);
+    texColor.a *= vAlpha;
+
+    if (texColor.a < 0.01) {
+      discard;
+    }
+
+    outColor = texColor;
 }
 `;
 
@@ -715,7 +721,7 @@ class InfiniteGridMenu {
     gl.enable(gl.CULL_FACE);
     gl.enable(gl.DEPTH_TEST);
 
-    gl.clearColor(0, 0, 0, 0);
+    gl.clearColor(0, 1, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     gl.uniformMatrix4fv(this.discLocations.uWorldMatrix, false, this.worldMatrix);
--- frontend/src/pages/auth/profile.jsx
+++ frontend/src/pages/auth/profile.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { useFirebaseStore } from '../../store/useFirebaseStore'
 import { Navigate, useNavigate } from 'react-router';
-
+import { Mail, User } from "lucide-react";
 
 function Profile() {
 
@@ -15,13 +15,55 @@ function Profile() {
     navigate("/home")
   }
 
+  if(!authUser){
+    return (<Navigate to="/home" replace={true} />)
+  }
 
   return (
     <div>
-      <br /><br /><br />
-      {authUser && <button onClick={handleClick}>logout</button>}
-      <br /><br /><br /><br /><br />
-    </div>
+
+    <div className="h-screen pt-20">
+      <div className="max-w-2xl mx-auto p-4 py-8">
+        <div className="bg-base-300 rounded-xl p-6 space-y-8">
+          <div className="text-center">
+            <h1 className="text-2xl font-semibold ">Profile Information</h1>
+          </div>
+
+          <div className="flex flex-col items-center gap-4">
+            <div className="relative">
+              <img
+                src={"/user/user.png"}
+                alt="Profile"
+                className="size-32 rounded-full object-cover border-4 "
+              />
+            </div>
+          </div>
+
+          <div className="space-y-6">
+            <div className="space-y-1.5">
+              <div className="text-sm text-zinc-400 flex items-center gap-2">
+                <User className="w-4 h-4" />
+                Full Name
+              </div>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser?.username}</p>
+            </div>
+
+            <div className="space-y-1.5">
+              <div className="text-sm text-zinc-400 flex items-center gap-2">
+                <Mail className="w-4 h-4" />
+                Email Address
+              </div>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser?.email}</p>
+            </div>
+          </div>
+        </div>
+        </div>
+          <button className='btn btn-active' onClick={handleClick}>logout</button>
+        </div>
+      </div>
+    
+
+    
   )
 }
 
