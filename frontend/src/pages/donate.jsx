c6dd85c5acb0cc1007757863379c89b30534e1d0
iramxx <ahmedmaghri4@gmail.com>
2025-08-06 13:15:55 +0100

    finished the payment logic

--- frontend/src/pages/donate.jsx
+++ frontend/src/pages/donate.jsx
@@ -1,13 +1,18 @@
 import DonationFrom from "../components/donation/DonationForm";
+import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise } from "../lib/stripe";
 
-function DonatePage(){
-    return(
-        <div>
-            <h1>Financial Transactions to the MSA.</h1>
-            <h2>Or support with us with a donation!</h2>
-            <DonationFrom/>
-        </div>
-    )
+function DonatePage() {
+  return (
+    <div>
+      <h1>Financial Transactions to the MSA.</h1>
+      <h2>Or support with us with a donation!</h2>
+
+      <Elements stripe={stripePromise}>
+        <DonationFrom />
+      </Elements>
+    </div>
+  );
 }
 
-export default DonatePage
+export default DonatePage;
