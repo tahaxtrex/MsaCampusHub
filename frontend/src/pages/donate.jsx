c0e8651348392fd9e32d5da09466c08d52acbd90
iramxx <ahmedmaghri4@gmail.com>
2025-08-06 13:31:15 +0100

    Payment implementation done, left: saving them in DB

--- frontend/src/pages/donate.jsx
+++ frontend/src/pages/donate.jsx
@@ -4,9 +4,9 @@ import { stripePromise } from "../lib/stripe";
 
 function DonatePage() {
   return (
-    <div>
-      <h1>Financial Transactions to the MSA.</h1>
-      <h2>Or support with us with a donation!</h2>
+    <div className="text-center mb-8">
+      <h1 className="text-4xl font-bold text-center text-green-900 mb-4"> Financial Transactions to the MSA.</h1>
+      <h2 className="text-center text-gray-600 text-lg max-w-2xl mx-auto mb-10">Or support with us with a donation!</h2>
 
       <Elements stripe={stripePromise}>
         <DonationFrom />
